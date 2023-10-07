90971a3da4500b0039006210fc1d72eda1c1e45b
jimmywachira <jmmndegwa@gmail.com>
2023-10-07 01:58:02 -0700

    using printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * print wth puts then return 0 
- * */
+* main - A progrm tht print code wth puts fxn
+* return : 0 (success)
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
