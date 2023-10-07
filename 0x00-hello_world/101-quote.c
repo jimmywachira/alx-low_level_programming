5357b00e6940c199be9b05719871a0191e279760
jimmywachira <jmmndegwa@gmail.com>
2023-10-07 04:41:53 -0700

    8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program to print something
+ *
+ * Returns: 0 on success, or a non-zero value on failure.
+ */
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+return (1);
+}
