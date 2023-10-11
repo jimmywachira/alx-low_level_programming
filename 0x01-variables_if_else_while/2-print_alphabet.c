77d7c173950a22411931bd6930ac9719f9d702c7
jimmywachira <jmmndegwa@gmail.com>
2023-10-11 07:36:38 -0700

    alphabets a-z

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 /**
- * main - A program to prints loweer chars:
+ * main - A program to print
  *
  * Returns: 0 on success, or a non-zero value on failure.
  */
 int main(void)
 {
+char c;
+c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
+return (0);
+}
