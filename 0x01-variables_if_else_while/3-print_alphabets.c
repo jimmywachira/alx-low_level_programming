0e23859e68a6edcaed6f26dc779d3f030c6333ea
jimmywachira <jmmndegwa@gmail.com>
2023-10-11 07:59:33 -0700

    alpABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - main file comment goes here
+ * betty style doc for function main goes there
+ * Returns: 0 on success, or a non-zero value on failure.
+ */
+int main(void)
+{
+char c, d;
+c = 'a';
+d = 'A';
+while (c <= 'z')
+{
+putchar(c);
+c++;
+}
+{
+while (d <= 'z')
+{
+putchar(d);
+d++;
+}
+putchar('\n');
+return (0);
+}
