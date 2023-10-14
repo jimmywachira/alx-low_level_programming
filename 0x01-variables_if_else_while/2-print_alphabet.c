7fcee2fdf4764296f8ded786ce7c993b2d569056
jimmywachira <jmmndegwa@gmail.com>
2023-10-14 04:56:13 -0700

    some commit message

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program
- * Return: 0 on success, or a non-zero value on failure.
+ * main - prints alphabets
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
