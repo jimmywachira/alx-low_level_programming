e88e90b8e8a5efd7d0b0b9dff2414860345c0b48
jimmywachira <jmmndegwa@gmail.com>
2023-10-28 04:06:47 -0700

    stat lib

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,22 +1,28 @@
 #include "main.h"
 
 /**
- * _memset - Program fills a block of memory with a specific value
- * @s: starting address of memory to be filled
- * @b: the desired value
- * @n: number of bytes to be changed
- * Return: change array with new value for n bytes
+ *  _strcat - function that concatenates two strings
+ *  @dest: entered value
+ *  @src: entered value
+ *  Return: void
  */
 
-char *_memset(char *s, char b, unsigned int n)
-
+char *_strcat(char *dest, char *src)
 {
-   	int a = 0;
-   	for (; n > 0; a++)
-   	{
-s[a] = b;
-n--;
+int a;
+int b;
+a = 0;
+while (dest[a] != '\0')
+{
+a++;
 }
-
-return (s);
+b = 0;
+while (src[b] != '\0')
+{
+dest[a] = src[b];
+a++;
+b++;
+}
+dest[a] = '\0';
+return (dest);
 }
