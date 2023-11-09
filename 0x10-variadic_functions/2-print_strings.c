046c321d1111d2a9cdde93db8f2def537c191648
jimmywachira <jmmndegwa@gmail.com>
2023-11-09 05:49:32 -0800

    Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,8 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-
-
 /**
 * print_strings - Prints strings, followed by a new line.
 * @separator: The string to be printed between strings.
@@ -30,59 +28,4 @@ printf("%s", separator);
 }
 printf("\n");
 va_end(strings);
-}
-
-
-
-
-=====================================
-
-3-print_all.c CODE
-
-
-
-#include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
-/**
-* print_all - prints anything
-* @format: list of types of arguments passed to the function
-*/
-void print_all(const char * const format, ...)
-{
-int i = 0;
-char *str, *sep = "";
-va_list list;
-va_start(list, format);
-if (format)
-{
-while (format[i])
-{
-switch (format[i])
-{
-case 'c':
-printf("%s%c", sep, va_arg(list, int));
-break;
-case 'i':
-printf("%s%d", sep, va_arg(list, int));
-break;
-case 'f':
-printf("%s%f", sep, va_arg(list, double));
-break;
-case 's':
-str = va_arg(list, char *);
-if (!str)
-str = "(nil)";
-printf("%s%s", sep, str);
-break;
-default:
-i++;
-continue;
-}
-sep = ", ";
-i++;
-}
-}
-printf("\n");
-va_end(list);
 }
